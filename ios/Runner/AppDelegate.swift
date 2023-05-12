77b0e81ade9f010e28f51b1dae2457e36ff4ffe1
nain93 <rnrb555@gmail.com>
2023-05-13 00:32:08 +0900

    feat: naver login 추가

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import NaverThirdPartyLogin
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,15 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    if let url = launchOptions?[.url] as? URL, url.absoluteString.contains("kakao"){
+      super.application(application, didFinishLaunchingWithOptions: launchOptions)
+      return true
+    } else if let url = launchOptions?[.url] as? URL, url.absoluteString.contains("naverlogin") {
+        NaverThirdPartyLoginConnection.getSharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        return true
+    } else {
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions) 
+    }
   }
 }
