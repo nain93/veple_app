0fe5f13703b67594f4b2d7ed7b88ed6de65ada68
nain93 <rnrb555@gmail.com>
2023-06-08 23:18:46 +0900

    chore: add showTopSheet:

--- lib/utils/general.dart
+++ lib/utils/general.dart
@@ -16,4 +16,59 @@ class General {
       enableDrag: false,
     );
   }
+
+  void showModalTopSheet(
+    BuildContext context,
+    String message,
+  ) {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      transitionDuration: const Duration(milliseconds: 500),
+      barrierLabel: MaterialLocalizations.of(context).dialogLabel,
+      barrierColor: Colors.black.withOpacity(0.5),
+      pageBuilder: (context, _, __) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: Card(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: <Widget>[
+                    ListTile(
+                      title: const Text('Item 1'),
+                      onTap: () => Navigator.of(context).pop('item1'),
+                    ),
+                    ListTile(
+                      title: const Text('Item 2'),
+                      onTap: () => Navigator.of(context).pop('item2'),
+                    ),
+                    ListTile(
+                      title: const Text('Item 3'),
+                      onTap: () => Navigator.of(context).pop('item3'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: CurvedAnimation(
+            parent: animation,
+            curve: Curves.easeOut,
+          ).drive(Tween<Offset>(
+            begin: const Offset(0, -1.0),
+            end: Offset.zero,
+          )),
+          child: child,
+        );
+      },
+    );
+  }
 }
